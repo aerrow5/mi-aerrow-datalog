2646d33932d46ea55362cd90a65a61c9828c39ea
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-16 18:18:49 +0100

    Hardcode script to correct node version

--- copy/var/zoneinit/includes/51-kibana.sh
+++ copy/var/zoneinit/includes/51-kibana.sh
@@ -10,14 +10,15 @@ pushd /opt/local
 tar xvf /tmp/kibana-${version}-linux-x64.tar.gz
 mv /opt/local/kibana-${version}-linux-x64 /opt/local/kibana
 
-# hardcode directory in kibana script:
-sed -i 's/\(NODE="${DIR}\)/DIR="\/usr" #hardcode in script\r\n\1/' kibana
+# hardcode node exec in kibana script:
+sed -i 's/\(test -x\)/NODE="\/opt\/local\/bin\/node" #hardcode solaris node in script \r\n\1/' kibana
+chown -R elastic:elastic /opt/local/kibana
 
-#test -d /var/tmp/elasticsearch || mkdir -p /var/tmp/elasticsearch && chown elastic /var/tmp/elasticsearch
+test -d /var/tmp/kibana || mkdir -p /var/tmp/kibana && chown elastic:elastic /var/tmp/kibana
 
 #mkdir /opt/local/UniFi/{logs,work} && chown unifi /opt/local/UniFi/{logs,work}
 
 popd
 
 # svccfg import not needed, as it is already done by mdata-init
-svcadm enable srv:/pkgsrc/elasticsearch:default
+svcadm enable svc:/pkgsrc/kibana:default
