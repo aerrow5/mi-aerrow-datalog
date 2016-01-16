99fccd92fcba89d92fa826a4cf48e1ec70594d87
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-16 15:46:45 +0100

    Create 50-elasticsearch.sh

---
+++ copy/var/zoneinit/includes/50-elasticsearch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script was tested with 2.2-1 so get that by default
+version=$(mdata-get elasticsearch:version || echo '2.1.1')
+
+pushd /tmp
+wget --no-check-certificate https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/$(version)/elasticsearch-$(version).tar.gz
+popd
+pushd /opt/local
+tar xvf /tmp/elasticsearch-$(version).tar.gz
+
+
+
+test -d /srv/unifi/data || mkdir -p /srv/unifi/data && chown unifi /srv/unifi/data
+test -d /srv/unifi/run || mkdir -p /srv/unifi/run && chown unifi /srv/unifi/run
+
+ln -s /srv/unifi/data /opt/local/UniFi/data
+ln -s /var/log/run /opt/local/UniFi/run
+
+mkdir /opt/local/UniFi/{logs,work} && chown unifi /opt/local/UniFi/{logs,work}
+
+popd
+
+# svccfg import not needed, as it is already done by mdata-init
+# svcadm enable svc:/network/unifi:default
